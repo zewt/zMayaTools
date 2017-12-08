25cd7f33f6b53f0e71613f6e12c286d87a3a4411
Glenn Maynard <glenn@zewt.org>
2017-12-08 16:54:05 -0600

    Use shared logging code in zOBBTransform.

--- plug-ins/zOBBTransform.py
+++ plug-ins/zOBBTransform.py
@@ -1,12 +1,13 @@
-import itertools, sys, logging
+import itertools, sys
 from pprint import pformat
 import maya.OpenMayaMPx as OpenMayaMPx
 import maya.OpenMaya as om
 import math, traceback, time
 
 from obb_transform import obb_transform
+from zMayaTools import maya_logging
 
-log = logging.getLogger('zOBBTransform')
+log = maya_logging.get_log()
 
 _rotation_modes = [
     'none',
@@ -14,35 +15,6 @@ _rotation_modes = [
     'primary',
     'secondary',
 ]
-
-class MayaLogHandler(logging.Handler):
-    def emit(self, record):
-        s = self.format(record)
-        if record.levelname == 'WARNING':
-            pm.warning(s)
-        elif record.levelname in ('ERROR', 'CRITICAL'):
-            # pm.error shows the error as red in the status bar, but it also only works if
-            # you let it throw an exception and kill your script.  It also shows a stack at
-            # the place it's called (here), which we don't want.  So, we need to use warning
-            # for errors.
-            pm.warning(s)
-        elif record.levelname == 'INFO':
-            print s
-
-        # Write all messages to sys.__stdout__, which goes to the output window.  Only write
-        # debug messages here.  The script editor is incredibly slow and can easily hang Maya
-        # for an hour if we have a lot of debug logging on, but the output window is reasonably
-        # fast.
-	sys.__stdout__.write('%s\n' % s)
-
-def setup_logging():
-    # Don't propagate logs to the root when we're in Maya.  Maya's default handler for outputting
-    # logs to the console is really ugly, so we need to override it.  Clear the handlers list before
-    # adding our own, in case this is a reload.
-    log.propagate = False
-    log.handlers = []
-    log.setLevel('DEBUG')
-    log.addHandler(MayaLogHandler())
 
 def iterate_array_handle(array):
     """
@@ -410,8 +382,6 @@ def creator():
     return OpenMayaMPx.asMPxPtr(zOBBTransform())
 
 def initialize():
-    setup_logging()
-
     mAttr = om.MFnMatrixAttribute()
     tAttr = om.MFnTypedAttribute()
     nAttr = om.MFnNumericAttribute()
