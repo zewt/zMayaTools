cdff66260833420c4bc39e3c44e272128a0e80c1
Glenn Maynard <glenn@zewt.org>
2017-12-15 22:23:27 -0600

    Fix error if install.bat is run when the Maya modules directory doesn't exist.

--- Install.bat
+++ Install.bat
@@ -5,6 +5,9 @@ FOR /F "tokens=2* delims= " %%A IN ('REG QUERY "HKCU\Software\Microsoft\Windows\
 set OUTPUT=%Documents%\maya\modules\zMayaTools.mod
 set INSTALL_DIR=%cd%
 
+rem Create the global modules directory if it doesn't exist.
+if not exist %Documents%\maya\modules mkdir %Documents%\maya\modules
+
 rem Create the .mod file in the user's Maya modules directory.
 echo + zMayaTools 1.0 %INSTALL_DIR% > %OUTPUT%
 echo MAYA_CUSTOM_TEMPLATE_PATH +:= scripts/NETemplates >> %OUTPUT%
