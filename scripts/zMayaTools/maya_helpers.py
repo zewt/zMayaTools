750433bed6eb130ed4804a92ccc1d5bb22b3464c
Glenn Maynard <glenn@zewt.org>
2019-03-18 14:53:15 -0500

    Add maya_helpers.Command (not currently used).

--- scripts/zMayaTools/maya_helpers.py
+++ scripts/zMayaTools/maya_helpers.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 from pymel import core as pm
 from maya import OpenMaya as om
 from zMayaTools import util, Qt
-import maya.OpenMayaUI as omui
+from maya import OpenMayaMPx as ompx, OpenMayaUI as omui
 from maya.api.MDGContextGuard import MDGContextGuard
 from maya import cmds
 
@@ -980,4 +980,45 @@ def open_scene_in_explorer():
         return
         
     util.show_file_in_explorer(scene_path)
+
+class Command(ompx.MPxCommand):
+    """
+    A base class for MPxCommands.
+    """
+    @classmethod
+    def register(cls, plugin):
+        plugin.registerCommand(cls.cmd, cls.create, cls.create_syntax)
+
+    @classmethod
+    def deregister(cls, plugin):
+        plugin.deregisterCommand(cls.cmd)
+
+    @classmethod
+    def create(cls):
+        return cls()
+
+    @classmethod
+    def create_syntax(cls):
+        return om.MSyntax()
+
+    def __init__(self):
+        super(Command, self).__init__()
+
+        # Set this to true after an action that can be undone.
+        self.undoable = False
+
+    def args(self, args):
+        """
+        Parse arguments and return an MArgParser.
+
+        If the arguments aren't valid, return None.  Maya will have printed an error, so
+        we should just stop if that happens.
+        """
+        try:
+            return om.MArgParser(self.syntax(), args)
+        except RuntimeError:
+            return None
+
+    def isUndoable(self):
+        return self.undoable
 
